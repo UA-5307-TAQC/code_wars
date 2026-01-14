fb3bfc45dfdd07a2e35b54dc439fcf65df9726f3
Liubov Titova <titova.l7789@gmail.com>
2026-01-14 16:07:06 +0200

    mistakes correction

--- kata/tliubov/eight.py
+++ kata/tliubov/eight.py
@@ -1,78 +1,80 @@
-from math import floor, sqrt, factorial
+"""8kyu level tasks."""
 
-# 1.Keep Hydrated!
+from math import factorial, floor, sqrt
+
+
 def litres(time):
+    """Keep Hydrated."""
     return floor(time * 0.5)
 
 
-# 2.Volume of a Cuboid
 def get_volume_of_cuboid(length, width, height):
+    """Volume of a Cuboid."""
     return length * width * height
 
 
-# 3.Miles per gallon to kilometers per liter
 def converter(mpg):
+    """Miles per gallon to kilometers per liter."""
     gallon_to_liters = 4.54609188
     mile_to_km = 1.609344
-    
-    kpl = mpg * (mile_to_km/gallon_to_liters)
-    return round(kpl, 2)   
+
+    kpl = mpg * (mile_to_km / gallon_to_liters)
+    return round(kpl, 2)
 
 
-# 4.To square(root) or not to square(root)
 def square_or_square_root(arr):
+    """To square(root) or not to square(root)."""
     squares = []
-    
+
     for num in arr:
         int_sqrt = floor(sqrt(num))
         if int_sqrt**2 != num:
             squares.append(num**2)
         else:
             squares.append(int_sqrt)
-        
+
     return squares
 
 
-# 5.Count of positives / sum of negatives
 def count_positives_sum_negatives(arr):
-    if len(arr) == 0:
+    """Count of positives / sum of negatives."""
+    if arr:
         return []
-    
+
     count = 0
-    sum = 0
-    
+    negative_sum = 0
+
     for num in arr:
         if num > 0:
             count += 1
         else:
-            sum += num
-        
-    return [count, sum]
+            negative_sum += num
+
+    return [count, negative_sum]
 
 
-# 6.Convert a String to a Number
 def string_to_number(s):
+    """Convert a String to a Number."""
     return int(s)
 
 
-# 7.Wilson primes
 def am_i_wilson(n):
+    """Wilson primes."""
     if n < 2:
         return False
-    
-    numerator = factorial(n-1) + 1
+
+    numerator = factorial(n - 1) + 1
     result = numerator / n**2
     if floor(result) == result:
         return True
     return False
 
 
-# 8.Formatting decimal places
 def two_decimal_places(n):
+    """Format decimal places."""
     return round(n, 2)
 
 
-# 9.Find numbers which are divisible by given number
 def divisible_by(numbers, divisor):
-    return [num for num in numbers if num % divisor == 0]
-
+    """Find numbers which are divisible by given number."""
+    return [num for num in numbers if not num % divisor]

2d84285e907eeebabcd7c32871a6bcf0d4804015
Valentayne <yehoianvb0510@gmail.com>
2026-01-14 11:46:27 +0200

    add 8kyu tasks

--- kata/valentyn_yehoian/eight.py
+++ kata/valentyn_yehoian/eight.py
@@ -1,14 +1,18 @@
 import math
 
+
 def litres(time):
     return math.floor(math.floor(time) * 0.5)
 
+
 def get_volume_of_cuboid(length, width, height):
-   return length * width * height
+    return length * width * height
+
 
 def square_or_square_root(arr):
-    result = [ int(math.sqrt(num)) if math.sqrt(num) == int(math.sqrt(num)) else num*num for num in arr ]
+    result = [int(math.sqrt(num)) if math.sqrt(num) == int(math.sqrt(num)) else num * num for num in arr]
     return result
+
 
 # def square_or_square_root(arr):
 #     result = []
@@ -16,8 +20,9 @@ def square_or_square_root(arr):
 #         if math.sqrt(num) == int(math.sqrt(num)):
 #             result.append(int(math.sqrt(num)))
 #         else:
-#             result.append(num*num)
+#             result.append(num * num)
 #     return result
+
 
 def count_positives_sum_negatives(arr):
     positive_numbers = 0
@@ -33,21 +38,25 @@ def count_positives_sum_negatives(arr):
             sum_of_negative += num
     return [positive_numbers, sum_of_negative]
 
+
 def string_to_number(s):
     return int(s)
+
 
 def am_i_wilson(n):
     if n < 2 or n > 999:
         return False
     return (math.factorial(n - 1) + 1) % (n * n) == 0
 
+
 def two_decimal_places(n):
-    return round(n,2)
+    return round(n, 2)
 
 
 def divisible_by(numbers, divisor):
     return [num for num in numbers if (num / divisor).is_integer()]
 
+
 def converter(mpg):
-    kpl = mpg * (1.609344/4.54609188)
-    return round(kpl,2)
+    kpl = mpg * (1.609344 / 4.54609188)
+    return round(kpl, 2)
--- kata/valentyn_yehoian/six.py
+++
@@ -1,9 +0,0 @@
-def find_nb(m):
-    i = 0
-    sum = 0
-    while sum < m:
-        i+= 1
-        sum+=i**3
-        if sum == m:
-            return i
-    return -1

a64d5a575fe33486197bd6b02f7b72ef96270f36
Valentayne <yehoianvb0510@gmail.com>
2026-01-21 19:59:02 +0200

    Collective approved program version

--- kata/main.py
+++ kata/main.py
@@ -1,15 +1,16 @@
 """Kata main file. Main function."""
-import kata.reader_runner
+
+import reader_runner
 
 
 def main():
     """Main function."""
     while True:
-        author = kata.reader_runner.Authors.choose_author()
-        type_choice = kata.reader_runner.Files.choose_file()
-        function_name = kata.reader_runner.Files.choose_function(type_choice)
+        author = reader_runner.Authors.choose_author()
+        type_choice = reader_runner.Files.choose_file()
+        function_name = reader_runner.Files.choose_function(type_choice)
 
-        kata.reader_runner.Runner.run_exercise(author, type_choice, function_name)
+        reader_runner.Runner.run_exercise(author, type_choice, function_name)
 
 
 if __name__ == "__main__":

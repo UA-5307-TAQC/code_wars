c80a8bf611a247998d6e5ba57a06c8b0763321af
Liubomyr halamaha <GALAMAGAL86@gmail.com>
2026-01-27 11:39:04 +0200

    update tests

--- tests/test_eight.py
+++ tests/test_eight.py
@@ -1,27 +1,6 @@
 """Formatting decimal places tests."""
 
-import importlib
-import os
-
 import pytest
-
-
-def get_module_paths():
-    """Create a list with module paths."""
-
-    modules = []
-    for package in os.listdir("kata"):
-        if package != "__pycache__":
-            modules.append(f"kata.{package}.eight")
-    return modules
-
-
-@pytest.fixture(name="import_eight", params=get_module_paths())
-def import_eighth_module(request):
-    """Import the eighth module."""
-
-    module = importlib.import_module(request.param)
-    return module
 
 
 @pytest.mark.parametrize(
@@ -38,8 +17,8 @@ def import_eighth_module(request):
         (1.99, 1.99),
     ],
 )
-def test_two_decimal_places(import_eight, value, expected):
+def test_two_decimal_places(eight_module, value, expected):
     """Run tests."""
 
-    result = import_eight.two_decimal_places(value)
+    result = eight_module.two_decimal_places(value)
     assert result == expected, f"{result} is expected to be {expected}"
--- tests/test_five.py
+++ tests/test_five.py
@@ -1,27 +1,6 @@
 """Gap in primes tests."""
 
-import importlib
-import os
-
 import pytest
-
-
-def get_module_paths(module: str = "five"):
-    """Create a list with module paths."""
-
-    modules = []
-    for package in os.listdir("kata"):
-        if package != "__pycache__":
-            modules.append(f"kata.{package}.{module}")
-    return modules
-
-
-@pytest.fixture(name="import_five", params=get_module_paths())
-def import_fifth_module(request):
-    """Import the fifth module."""
-
-    module = importlib.import_module(request.param)
-    return module
 
 
 @pytest.mark.parametrize(
@@ -35,8 +14,8 @@ def import_fifth_module(request):
         (2, 100, 103, [101, 103]),
     ],
 )
-def test_gap(import_five, gap, start, end, expected):
+def test_gap(five_module, gap, start, end, expected):
     """Run tests."""
 
-    result = import_five.gap(gap, start, end)
+    result = five_module.gap(gap, start, end)
     assert result == expected, f"{gap}, {start}, {end} is expected to be {expected}"

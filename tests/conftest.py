9eee4023e4d6e1967558a3347512e80f243e12cc
Liubomyr halamaha <GALAMAGAL86@gmail.com>
2026-01-29 10:27:17 +0200

    clear conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,6 @@ from typing import List, Literal, TypeAlias
 
 import pytest
 
-# from pathlib import Path
-# import  sys
-# sys.path.insert(0, str(Path(__file__).parent.parent))
-
-
 ModuleName: TypeAlias = Literal["five", "six", "seven", "eight"]
 
 
@@ -42,4 +37,3 @@ def _create_module_fixture(module_name: ModuleName):
 module_names: List[ModuleName] = ["five", "six", "seven", "eight"]
 for mod_name in module_names:
     globals()[f"fixture_import_{mod_name}"] = _create_module_fixture(mod_name)
-
